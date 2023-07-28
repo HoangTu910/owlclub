28f88c134f9c4cb9d621d87136ff09868712a026
HoangTu910 <101467222+HoangTu910@users.noreply.github.com>
2023-07-28 14:10:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var image = document.getElementById("image")
+        image.addEventListener("mouseover", function() {
+            
+        image.style.width = "170px"
+        image.style.height = "170px"
+    });
+
+var image = document.getElementById("image")
+    image.addEventListener("mouseout", function() {
+    image.style.height = "150px"    
+    image.style.width = "150px"
+});
